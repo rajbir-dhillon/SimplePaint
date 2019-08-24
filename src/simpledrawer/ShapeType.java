b01fd31cc6b3f665ab30d44d6effc1ec5a8d69da
rajbir-dhillon <rajbir.dhillon@hotmail.co.uk>
2019-08-24 21:10:34 +0100

    Uploaded

---
+++ src/simpledrawer/ShapeType.java
@@ -0,0 +1,12 @@
+/**
+ * ShapeType
+ *
+ *
+ * An enumerated type representing valid types of shape.
+ */
+
+package simpledrawer;
+
+public enum ShapeType {
+        LINE, OVAL, TRIANGLE, QUADRILATERAL;
+}
